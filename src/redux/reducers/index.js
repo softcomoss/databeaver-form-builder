a7d60663af1db51c198e9692f7e64eae22c8e2c0
Lucky Amadi <lucky@softcom.ng>
2020-06-06 06:04:33 +0100

    stripped unwanted reducers

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,20 +1,6 @@
-import { combineReducers } from "redux";
-import formReducer from "./form.reducer";
-import auth from "./auth.reducer";
-import tabActivitiesReducer from "./tab-activity.reducer";
-import agents from "./agents.reducer";
-import projects from "./project.reducer";
-import settingsReducer from "./settings.reducer";
-import client from "./client.reducer";
-import dashboard from "./dashboard.reducer";
+import { combineReducers } from 'redux'
+import formReducer from './form.reducer'
 
 export default combineReducers({
-	formReducer,
-	auth,
-	tabActivitiesReducer,
-	agents,
-	projects,
-	settingsReducer,
-	client,
-	dashboard,
-});
+  formReducer
+})
